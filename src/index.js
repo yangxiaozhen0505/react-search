fe301ce3f690165bc6db68000807857efe202399
yangxiaozhen0505 <736453008@qq.com>
2017-07-12 00:11:57 +0800

    'react-sliders'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Slider from './components/Slider';
+let IMAGE_DATA = [
+    {src: require('./images/1.jpg')},
+    {src: require('./images/2.png')},
+    {src: require('./images/3.png')}
+];
 /**
- * Created by Administrator on 2017/7/11.
+ * speed 图片的轮播速度
+ * delay 延迟时间，多长时间轮播一次
+ * pause 暂停，如果它为true,则当鼠标划过的时候自动停止轮播
+ * auto 当页面加载完成后自动开始轮播
+ * dots 是显示点状导航
+ * arrows 是否显示左右的箭头导航
  */
-//redux react redux-thunk redux-logger react-redux
-import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import App from './components/App'
-import store from './store';
-
-
-render(<Provider store = {store}><App/></Provider>,document.querySelector('#root'));
+render(
+    <Slider
+        items={IMAGE_DATA}
+        speed={1.2}
+        delay={2.1}
+        pause={true}
+        auto={true}
+        dots={true}
+        arrows={true}
+    />, window.app);
--- src/store/action-types.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Administrator on 2017/7/11.
- */
-export const SEARCH = 'SEARCH';
-export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
